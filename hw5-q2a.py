4ff7c8510d3d3434f48825dc66558328489f405e
Nampally Lakshmi Prasad <61342127+Lakshmi-14@users.noreply.github.com>
2022-11-16 11:52:18 +0530

    Update hw5-q2a

--- hw5-q2a.py
+++ hw5-q2a.py
@@ -1,20 +1,25 @@
+# Define Network Class
 class Network:
     def __init__(self, node_count):
         self.n_count = node_count
         self.network = list()
         self.nodes = list()
-
+    
+    # Add nodes to the network
     def add_vertex(self, ver_val):
         self.nodes.append(ver_val)
-
+    
+    # Add connections/edges to the network using weights
     def add_connection(self, source, dest, weight):
         self.network.append([source, dest, weight])
-
+    
+    # DIsplay Result
     def disp_short_weights(self, source, network_map):
         print("Shortest-path tree using Bellman-Ford algorithm")
         for (node, weight) in network_map.items():
             print(source, "->", node.ljust(3), ":", weight)
-
+    
+    # Implementation of Bellman-ford Algorithm
     def bellManFord(self, source):
         net_source = source
         dist = {node: float("inf") for node in self.nodes}
@@ -34,14 +39,19 @@ class Network:
 
 
 if __name__ == "__main__":
+    
+    # Initializing network
     net = Network(6)
+    
+    # Adding vertices to the network
     net.add_vertex("S")
     net.add_vertex("A")
     net.add_vertex("B")
     net.add_vertex("C")
     net.add_vertex("D")
     net.add_vertex("t")
-
+    
+    # Adding COnnections to the network
     net.add_connection("S", "A", 4)
     net.add_connection("S", "B", 6)
     net.add_connection("A", "D", 1)
@@ -51,8 +61,11 @@ if __name__ == "__main__":
     net.add_connection("C", "D", 1)
     net.add_connection("C", "t", 3)
     net.add_connection("D", "t", 7)
-
+    
+    # Performing the Bellman-Ford algorithm
     net.bellManFord("S")
+    
+    input("\n\n\n Press Enter to exit")
 
 
 '''
